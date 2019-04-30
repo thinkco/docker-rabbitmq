89ef37e4712e393441e25eb6008eb84f5cdb906b
Carlos Lozano <carlos@adaptive.me>
2019-04-30 14:25:46 +0200

    Delete guest user by default.

--- init.sh
+++ init.sh
@@ -7,7 +7,8 @@ rabbitmqctl set_user_tags $RABBITMQ_ADMIN_USER administrator ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_ADMIN_USER  ".*" ".*" ".*" ; \
 rabbitmqctl add_user $RABBITMQ_API_USER $RABBITMQ_API_PASSWORD 2>/dev/null ; \
 rabbitmqctl set_user_tags $RABBITMQ_API_USER management ; \
-rabbitmqctl set_permissions -p / $RABBITMQ_ADMIN_USER  ".*" ".*" ".*" ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_API_USER  ".*" ".*" ".*" ; \
+rabbitmqctl delete_user guest ; \
 echo "*** Admin User: '$RABBITMQ_ADMIN_USER' with password '$RABBITMQ_ADMIN_PASSWORD' completed. ***" ; \
 echo "*** API User:  '$RABBITMQ_API_USER' with password '$RABBITMQ_API_PASSWORD' completed. ***" ; \
 echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
